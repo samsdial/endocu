8e7027f8790263bd632afb2c085c42f86eeb3270
Sergio Martinez <dialstudios@gmail.com>
2018-11-19 21:46:03 -0500

    initial Commit

---
+++ README.md
@@ -0,0 +1,166 @@
+﻿# Sams WorkFlow 2017
+
+Gulp wordFlow 2017
+
+* [Gulp] - Automatizador de tareas
+br
+* [Sass] - Extención minificación de css como preprocesador
+br
+* [sourcemaps] - Maping como herramienta de rastreo luego de la compilación tanto de las hojas de estilo css, al igual que todos los archivos javascript.
+br
+* [Autoprefixer] - Esencial en la compilación de los estilos para el soporte de los prefijos de todos los navegadores.
+br
+* [imagemin] - Minificador de imagenes
+br
+* [uglify] -
+br
+* [babel] -
+br
+* [pug] -
+br
+* [BrowserSync] -
+
+
+*** Install
+```shell
+$ npm install
+```
+
+*** Running
+```shell
+$ gulp watch
+```
+
+
+# Que es e VUE.JS
+# Que es data bing y reactividad
+
+  - Que pasa cuando el modelo cambia o se actualiza? deberia Actualizarse... Reactividad 
+
+# Que son las directivas
+   
+   - atributos y capacidades a los html 
+
+***  Renderisado condicional
+ 
+v show
+    - Elemento que se puede mostra o ocultar(display none) con una variable booleana
+```javascript
+    v-show
+```
+v if
+    - Elemento que se puede borrar o crear en el Dom apartir de una varia ble booleana
+
+v else
+    - Elemento se puede condicionar segú el valor conducional de una variable
+```html
+    <main>
+        <h1 v-if="edad < 18">No puedes entrar.</h1>
+        <h1 v-else-if="edad > 200"> Eres inmortal.</h1>
+        <h1 v-else> Puedes entrar.</h1>
+    </main>
+```
+````javascript
+    const vm = new Vue ({
+        el: 'main',
+        data: {
+            conectado: false,
+            edad: 44,
+        }
+    });
+````
+*** Template
+
+```html
+    <main>
+        <template v-if="conectado">
+        <h3> Bievenido Juan</h3>
+        <ul>
+        <li><a href="#">Mis datos</a></li>
+        <li><a href="#">Mensaje</a></li>
+        <li><a href="#">Salir</a></li>
+        </ul>
+        </template>
+    </main>
+```
+kloustr <gerencia@kloustr.com>
+	
+1 nov. 2018 20:17 (hace 18 horas)
+	
+para mí
+HOME
+Slider
+	1.  Incrustar las imágenes en la resolución correcta que corresponden a cada slide.  Yo creería que con 5 segundos entre cada uno estaría bien.
+
+	2. El gradiente de los azules del slider sigue siendo el incorrecto.
+	3. La transición del texto y las imágenes planteadas aún no está, pero si es demasiado difícil de hacer, dejarlas así. 
+	
+About Condor Box
+	1. La animación del slider de los productos no corresponde a la planteada.
+
+Así de fácil
+
+ok
+Hemos diseñado
+ok
+Calculadora
+ok
+
+
+Preguntas frecuentes - Contacto
+1. Permitir la rotación automática de la Preguntas Frecuentes en el gradiente. Que no solo aparezca la pregunta de Amazon-Ebay. 
+
+Descubre miles de productos
+
+1. La animación de los logos que lleguen todos desde abajo, no unos desde arriba y otros desde abajo. 
+2. Aquí están los hipervinculo de las páginas que irían en ese slider de recomendaciones:
+https://www.amazon.com
+https://www.bestbuy.com
+https://www.ebay.com
+https://www.target.com
+https://www.walmart.com
+https://www.nike.com/us/en_us/
+https://www.perryellis.com
+https://www.basspro.com
+https://www.journeys.com
+https://explore.calvinklein.com/en_US/explore
+https://www2.hm.com/en_us/index.html
+https://ww.victoriassecret.com
+https://www.brandsmartusa.com
+https://www.michaelkors.com
+https://www.apple.com
+Esto dicen sobre Condor Box
+1. La animación de los logos que lleguen todos desde abajo, no unos desde arriba y otros desde abajo. 
+2. Este slider de artículos también es con autoplay.
+Footer
+1. Separar más los iconos de redes sociales.
+2. Corregir ortografía sobre Imperio Cargo Express
+
+
+Preguntas frecuentes
+1.  La barra de arriba aparece blanca cuando está sobre el gradiente. Esta debe aparecer transparente igual que aparece sobre el slider del home o en la página de contacto. 
+2. El footer aparece vacío
+
+Contacto
+1.  Eliminar el recuadro de Contacto en la página de Contacto abajo del número de wpp pues queda redundante.
+
+Rastrear 
+1.  Cambiar la animación del giro del recuadro. Que llegue desde abajo con opacidad así como las otras. 
+
+Iniciar sesión
+1.  Quitar el titular de Ingresa a tu casillero virtual, ya que está también en recuadro de MIISIL, al plantear el diseño creí que se podría quitar del MIISIL. 
+
+Registro
+1.  La barra de arriba aparece blanca cuando está sobre el gradiente. Esta debe aparecer transparente igual que aparece sobre el slider del home o en la página de contacto (la misma corrección de FAQs).
+
+Responsive
+1.  Favor hacer Responsive las ventanas de MISIIL: Rastreo, Iniciar Sesión, Registro. 
+2. Mejorar aspectos generales cuando está en la pantalla de un celular (Botones sobre montados, partes que no aparecen como el de los artículos). Aplica para el Home y las páginas internas.
+
+Anotaciones adicionales
+1. Sobre la configuración de la ventana de Contacto el correo al que deben llegar todos los mensajes de los clientes es info@condorbox.com y Daniel ya te pasará el correo automático que le debe llegar al cliente una vez ha mandado el mensaje. Me cuentas cuánto vale esto por favor pero ayúdame que es muy importante. 
+2. Cotízame también por favor la opción de que aparezcan como deben ser los mensajes de Facebook de nuestros clientes. Esto lo podemos hacer luego 
+3. Cuánto crees que puedas tardarte en estas correcciones? 
+4. Podríamos retrasar la subida de la página hasta mañana a la media noche. Lo crees posible?
+
+2. Las animaciones de entrada están muy lentas. PROCURAR QUE TODAS LAS ANIMACIONES DEL HOME TENGAN LA MISMA VELOCIDAD, como la velocidad del titular #ConsumoInteligente, ese está perfecto. 
---
+++ src/js/custom.js
@@ -0,0 +1,260 @@
+$(document).ready(function($){
+    $(function() {
+        var a, e, t;
+        return $("ul.navbar-nav li a[href*=#]").click(function() {
+            var a, e;
+            return location.pathname.replace(/^\//, "") === this.pathname.replace(/^\//, "") && location.hostname === this.hostname && (a = $(this.hash),
+                a = a.length && a || $("[name=" + this.hash.slice(1) + "]"),
+                a.length) ? (e = a.offset().top - 98,
+                $("html,body").animate({
+                    scrollTop: e
+                }, 1e3),
+                !1) : void 0
+        }),
+            t = window.location.href.split("#"),
+            window.location.href[1] && (a = $("#" + t[1]),
+                a.length) ? (e = a.offset().top - 0,
+                $("html,body").animate({
+                    scrollTop: e
+                }, 1e3),
+                !1) : void 0
+    });
+    $('.slider_intro').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        //autoplay: true,
+        //autoplaySpeed: 4000,
+        //asNavFor: '.slider-nav'
+    });
+    $('.slider-nav').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.slider_intro',
+        dots: true,
+        centerMode: true,
+        focusOnSelect: true,
+        arrows: false
+    });
+    $('.slider_recommend').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    autoplay: true,
+                    autoplaySpeed: 4000,
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    autoplay: true,
+                    autoplaySpeed: 4000,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    autoplay: true,
+                    autoplaySpeed: 4000,
+                }
+            }
+        ]
+    });
+    //.slider_quote_people
+    $('.slider_quote_people').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        arrows: false,
+        responsive: [
+            {
+                breakpoint: 1300,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+    $('.slider_quote_medios').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        eautoplaySpeed: 5000,
+    });
+    $('.slider_about').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        eautoplaySpeed: 4000,
+    });
+    $(document).on("scroll", function(){
+        if
+        ($(document).scrollTop() > 50){
+            $("#header").addClass("");//navbar_onscroll
+        }
+        else
+        {
+            $("#header").removeClass("");//navbar_onscroll
+        }
+    });
+    $(document).on("scroll", function(){
+        if
+        ($(document).scrollTop() > 50){
+            $("#logo").addClass("logo_cobox--color");
+        }
+        else
+        {
+            $("#logo").removeClass("logo_cobox--color");
+        }
+    });
+    // Calculadora
+    $('#calculate').click(function(e) {
+        var weight = $('#weight').val();
+        var declared = $('#declared').val();
+        var total = 0;
+        var percentage = 0;
+        if(weight != undefined && weight != null && weight != "" &&
+            declared != undefined && declared != null && declared != "") {
+            if(weight > 0 && declared > 0) {
+                if(weight > 110) {
+                    //alert('el peso máximo permitido por guía no puede superar las 110 libras.');
+                    $(".weight + .error").addClass("i");
+                    $("#weight").focus();
+                    $(".realvalue + .alerta").removeClass("i");
+                    $(".diligence + .alerta").removeClass("a");
+                }else{
+                    weight < 7 ? weight = 7 : weight = weight;
+                    declared < 30 ? declared = 30 : declared = declared;
+                    declared <=200 ? percentage = 0.14 : percentage = 0.30;
+                    total = ( (weight*1.70) + (declared*percentage) + 5 );
+                    total = (Math.round(total*100)/100).toFixed(1);
+                    $('#total').text('$'+total);
+                }
+            }else{
+                //alert('Los valores deben ser superiores a 0');
+                $(".realvalue + .alerta").addClass("i");
+                $(".diligence + .alerta").removeClass("a");
+                $(".weight + .error").removeClass("i");
+            }
+        }else{
+            //alert('Diligenciar los campos');
+            $(".diligence + .alerta").addClass("a");
+            $(".weight + .error").removeClass("i");
+            $(".realvalue + .alerta").removeClass("i");
+        }
+    });
+    //Expaden Checked true
+    $('#expanded').change(function() {
+        $('#checkboxdiv').toggle();
+    });
+    // Validacion FF
+    $('#submit').click(function(){
+        var name        = $("#name").val();
+        var email       = $("#email").val();
+        var validacion_email = /^[a-zA-Z0-9_\.\-]+@[a-zA-Z0-9\-]+\.[a-zA-Z0-9\-\.]+$/;
+        var phone       = $("#phone").val();
+        var city       = $("#city").val();
+        var message    = $("#message").val();
+
+        if(name == "" || email == "" || phone == "" || city == "" || message == "" ){
+            if(name == ""){
+                $(".name + .error").addClass("i");
+                $("#name").focus();
+                return false;
+            }else{
+                $(".name + .error").removeClass("i");
+            }
+            if(email == "" || !validacion_email.test(email)){
+                $(".email + .error").addClass("i");
+                $("#email").focus();
+                return false;
+            }else{
+                $(".email + .error").removeClass("i");
+            }
+            if(phone == ""){
+                $(".cel + .error").addClass("i");
+                $("#phone").focus();
+                return false;
+            }else{
+                $(".cel + .error").removeClass("i");
+            }
+            if(city == ""){
+                $(".city + .error").addClass("i");
+                $("#city").focus();
+                return false;
+            }else{
+                $(".city + .error").removeClass("i");
+            }
+            if(message == ""){
+                $(".message + .error").addClass("i");
+                $("#message").focus();
+                return false;
+            }else{
+                $(".message + .error").removeClass("i");
+            }
+        }else{
+            $(".message + .error").removeClass("i");
+            var datos = '&name=' + name +
+                '&email=' + email +
+                '&phone=' + phone +
+                '&city=' + city+
+                '&message=' + message;
+            $.ajax({
+                method:"Post",
+                url:"contact.php",
+                dataType:"json",
+                data:datos,
+            }).done(function( msg ){
+                if(msg.success){
+                    $('.alerta p').fadeIn("slow");
+                    $('.alerta p').html('Muchas Gracias por contáctarnos, pronto nos estaremos comunicando contigo.');
+                    $('#name').val("");
+                    $('#email').val("");
+                    $('#phone').val("");
+                    $('#city').val("");
+                    $('#message').val("");
+                }else{
+                    $('.alerta p').fadeIn("slow");
+                    $('.alerta p').html('Error, intente más tarde por favor.');
+                }
+            });
+            return false;
+        }
+    });
+});
+AOS.init();
